a6415c2537bc89da62aae66e49d7f1d6be531805
Oskk22 <105198327+Oskk22@users.noreply.github.com>
2022-06-08 13:44:16 +0200

    Initial commit

---
+++ HelloWorld/src/ejercicio344.java
@@ -0,0 +1,38 @@
+
+public class ejercicio344 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Ejercicio 1
+		int a=1,b=2,c=3,d=4;
+		if (a+b > c+d) {
+			System.out.println("a+b es mayor que c+d");
+		}
+		
+		//Ejercicio 2
+		int angulo1=70, angulo2=50, angulo3=60;
+		if (angulo1+angulo2+angulo3 ==180) {
+			System.out.println("Los angulos forman un triangulo");
+		}
+		
+		//Ejercicio 3
+		int a3=16;
+		if (a3%2==0) {
+			System.out.println("El valor de a es una cifra par");
+		}
+		
+		//EJERCICIO 348
+		int num1=1, num2=2, num3=3;
+		if (num1>num2 && num1>num3) {
+			System.out.println("La variable num1 es el mayor");
+		}
+		else if (num2>num3) {
+			System.out.println("La variable num2 es el mayor");
+		}
+		else {
+			System.out.println("La variable num3 es la mayor");
+		}
+	}
+	
+
+}
